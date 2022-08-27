5e146d6007ae5d01ab6425a67240c36ba6b2c29b
Micoder-dev <micoder.com@gmail.com>
2022-08-27 15:48:42 +0530

    play like curl implemented

--- app/src/main/java/com/example/pagecurl_api/MainActivity.java
+++ app/src/main/java/com/example/pagecurl_api/MainActivity.java
@@ -2,13 +2,75 @@ package com.example.pagecurl_api;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.MotionEvent;
+import android.view.View;
+
+import karacken.curl.PageCurlAdapter;
+import karacken.curl.PageSurfaceView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private PageSurfaceView pageSurfaceView;
+    private int screen_width;
+    private int screen_height;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+
+        pageSurfaceView = new PageSurfaceView(this);
+
+        setContentView(pageSurfaceView);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point(); display.getSize(size);
+        screen_width = size.x;  screen_height = size.y;
+        String[] res_array=null;
+        if(getResources().getConfiguration().orientation== Configuration.ORIENTATION_PORTRAIT)
+            res_array=  new String[]{"page1.png", "page2.png", "page3.png"};
+        else
+            res_array=  new String[]{"page1.png", "page2.png", "page3.png"};
+
+        PageCurlAdapter pageCurlAdapter=new PageCurlAdapter(res_array);
+        pageSurfaceView.setPageCurlAdapter(pageCurlAdapter);
+
     }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        pageSurfaceView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        pageSurfaceView.onPause();
+    }
+
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return pageSurfaceView.onPageTouchEvent(event);
+    }
+
 }
